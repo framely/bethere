a3073d0c43a695395631936bb1dd5c01cd0f6ee4
Sunny <hisunnymay@163.com>
2024-03-21 14:58:20 +0800

    hide blog

--- docs/.vitepress/configs/navbar/en.ts
+++ docs/.vitepress/configs/navbar/en.ts
@@ -9,10 +9,5 @@ export const en = [
     text: 'Pricing',
     link: '/pricing/',
     activeMatch: '/pricing/',
-  },
-  {
-    text: 'Blog',
-    link: '/articles/',
-    activeMatch: '/articles/',
   }
 ]
