d0f0e62313db6b1a32c9ca7d7d2ba08a8105ec91
xiaoyun wu <xiaoyun.wu@gmail.com>
2026-07-02 19:40:30 -0700

    Add homepage three-step flow

--- docs/.vitepress/theme/home-hero-after.vue
+++ docs/.vitepress/theme/home-hero-after.vue
@@ -2,19 +2,11 @@
 import { computed } from 'vue'
 import { useData } from 'vitepress'
 import PromptHero from '../../components/home/PromptHero.vue'
-import HomeVideo from '../../components/home/HomeVideo.vue'
 
 const { frontmatter } = useData()
 const showPromptHero = computed(() => frontmatter.value.promptHero === true)
-const homeVideo = computed(() => frontmatter.value.homeVideo)
-const homeVideoPoster = computed(() => frontmatter.value.homeVideoPoster)
 </script>
 
 <template>
   <PromptHero v-if="showPromptHero" />
-  <HomeVideo
-    v-if="homeVideo"
-    :src="homeVideo"
-    :poster="homeVideoPoster"
-  />
 </template>
